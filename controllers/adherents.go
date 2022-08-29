3bc7b85087f0f47054c025a276f857310678b572
Mica <mika89700@gmail.com>
2022-08-29 22:11:19 +0200

    add logger

--- controllers/adherents.go
+++ controllers/adherents.go
@@ -3,10 +3,10 @@ package controllers
 import (
 	"encoding/json"
 	"io"
-	"log"
 	"net/http"
 	"strconv"
 
+	"github.com/ESNFranceG33kTeam/sAPI/logger"
 	"github.com/ESNFranceG33kTeam/sAPI/models"
 	"github.com/gorilla/mux"
 )
@@ -17,7 +17,9 @@ func AdherentsIndex(w http.ResponseWriter, r *http.Request) {
 
 	err := json.NewEncoder(w).Encode(models.AllAdherents())
 	if err != nil {
-		log.Fatal(err)
+		logger.LogError("adherent", "problem with indexation.", err)
+	} else {
+		logger.LogInfo("adherent", "request GET : "+r.RequestURI)
 	}
 }
 
@@ -27,21 +29,23 @@ func AdherentsCreate(w http.ResponseWriter, r *http.Request) {
 
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
-		log.Fatal(err)
+		logger.LogError("adherent", "problem with create.", err)
+	} else {
+		logger.LogInfo("adherent", "request POST : "+r.RequestURI)
 	}
 
 	var adh models.Adherent
 
 	err = json.Unmarshal(body, &adh)
 	if err != nil {
-		log.Fatal(err)
+		logger.LogError("adherent", "problem with unmarshal.", err)
 	}
 
 	models.NewAdherent(&adh)
 
 	err = json.NewEncoder(w).Encode(adh)
 	if err != nil {
-		log.Fatal(err)
+		logger.LogError("adherent", "problem with encoder.", err)
 	}
 }
 
@@ -52,14 +56,16 @@ func AdherentsShow(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id, err := strconv.Atoi(vars["id"])
 	if err != nil {
-		log.Fatal(err)
+		logger.LogError("adherent", "unable to get id.", err)
+	} else {
+		logger.LogInfo("adherent", "request GET : "+r.RequestURI)
 	}
 
 	adh := models.FindAdherentById(id)
 
 	err = json.NewEncoder(w).Encode(adh)
 	if err != nil {
-		log.Fatal(err)
+		logger.LogError("adherent", "problem with encoder.", err)
 	}
 }
 
@@ -70,26 +76,28 @@ func AdherentsUpdate(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id, err := strconv.Atoi(vars["id"])
 	if err != nil {
-		log.Fatal(err)
+		logger.LogError("adherent", "unable to get id.", err)
 	}
 
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
-		log.Fatal(err)
+		logger.LogError("adherent", "problem with update.", err)
+	} else {
+		logger.LogInfo("adherent", "request PUT : "+r.RequestURI)
 	}
 
 	adh := models.FindAdherentById(id)
 
 	err = json.Unmarshal(body, &adh)
 	if err != nil {
-		log.Fatal(err)
+		logger.LogError("adherent", "problem with unmarshal.", err)
 	}
 
 	models.UpdateAdherent(adh)
 
 	err = json.NewEncoder(w).Encode(adh)
 	if err != nil {
-		log.Fatal(err)
+		logger.LogError("adherent", "problem with encoder.", err)
 	}
 }
 
@@ -102,11 +110,11 @@ func AdherentsDelete(w http.ResponseWriter, r *http.Request) {
 	// strconv.Atoi is shorthand for ParseInt
 	id, err := strconv.Atoi(vars["id"])
 	if err != nil {
-		log.Fatal(err)
+		logger.LogError("adherent", "unable to get id.", err)
 	}
 
 	err = models.DeleteAdherentById(id)
 	if err != nil {
-		log.Fatal(err)
+		logger.LogError("adherent", "unable to delete adherent.", err)
 	}
 }
