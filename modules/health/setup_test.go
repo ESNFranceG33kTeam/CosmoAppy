8ded8bd1c33c09f5a05de213688471dc8b60eba6
Mica <mika89700@gmail.com>
2022-11-21 11:57:44 +0100

    add init tests

---
+++ modules/health/setup_test.go
@@ -0,0 +1,36 @@
+package health
+
+import (
+	"log"
+	"os"
+	"testing"
+
+	"github.com/ESNFranceG33kTeam/CosmoAppy/database"
+	"github.com/ESNFranceG33kTeam/CosmoAppy/helpers"
+)
+
+func TestMain(m *testing.M) {
+	testMainSetup()
+
+	//log.Println("Do stuff BEFORE the tests!")
+	exitVal := m.Run()
+	//log.Println("Do stuff AFTER the tests!")
+	testMainTeardown()
+	os.Exit(exitVal)
+
+}
+
+func testMainSetup() {
+	helpers.Confpathflag = "../../test/conf_local.yaml"
+	helpers.InitFile()
+	helpers.ReadConfig()
+	TheLogger().LogInit()
+	database.DatabaseInit()
+}
+
+func testMainTeardown() {
+	_, err := TheDb().Exec("DROP DATABASE IF EXISTS " + helpers.TheAppConfig().Namedb + ";")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
