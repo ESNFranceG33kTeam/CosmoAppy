c70c4d05a7183bb79ab1b798a6007e5cffde5648
Mica <mika89700@gmail.com>
2023-01-20 18:06:13 +0100

    add fields to volunteer

--- modules/volunteer/README.md
+++ modules/volunteer/README.md
@@ -6,12 +6,17 @@ title: Module Volunteer
 
 **Object description**
 
-- Id            int     `json:"id"`             - Id of the volunteer
-- Firstname     string  `json:"firstname"`      - Firstname of the volunteer
-- Lastname      string  `json:"lastname"`       - Lastname of the volunteer
-- Email         string  `json:"email"`          - Email of the volunteer
-- Actif         bool    `json:"actif"`          - Status of the volunteer
-- Bureau        bool    `json:"bureau"`         - Does the volunteer has a bureau role
+- Id                int     `json:"id"`             - Id of the volunteer
+- Firstname         string  `json:"firstname"`      - Firstname of the volunteer
+- Lastname          string  `json:"lastname"`       - Lastname of the volunteer
+- Email             string  `json:"email"`          - Email of the volunteer
+- Discord           string  `json:"discord"`        - Discord pseudo
+- Phone             string  `json:"phone"`          - Phone number
+- University        string  `json:"university"`     - University name
+- Postal address    string  `json:"postal_address"` - Postal address
+- Actif             bool    `json:"actif"`          - Status of the volunteer
+- Bureau            bool    `json:"bureau"`         - Does the volunteer has a bureau role
+- Started date      string  `json:"started_date"`   - Date of volunteering started
 
 The parameters `id` is calculated automatically.
 
@@ -48,8 +53,13 @@ curl -X POST "https://${MYSERVER}/${SECURE}/volunteers" \
         "firstname": "Ash",
         "lastname": "Ketchum",
         "email": "ash.ketchum@indego.com"
+        "discord": "Alola",
+        "phone": "0123456789",
+        "university": "Indigo",
+        "postal_address": "01 bis house, 00001 Pallet Town, Kanto",
         "actif": true,
-        "bureau": false
+        "bureau": false,
+        "started_date": "1997-04-01"
     }'
 ```
 
@@ -67,9 +77,14 @@ curl -X POST "https://${MYSERVER}/${SECURE}/volunteers/2" \
     -d '{
         "firstname": "Ash",
         "lastname": "Ketchum",
-        "email": "ash.ketchum@indego.com"
+        "email": "ash.ketchum@indego.com",
+        "discord": "Alola",
+        "phone": "0123456789",
+        "university": "Indigo",
+        "postal_address": "01 bis house, 00001 Pallet Town, Kanto",
         "actif": false,
-        "bureau": false
+        "bureau": false,
+        "started_date": "1997-04-01"
     }'
 ```
 
--- modules/volunteer/model.go
+++ modules/volunteer/model.go
@@ -14,19 +14,34 @@ type Volunteer struct {
 	// Email of the volunteer
 	// in: string
 	Email string `json:"email"`
+	// Discord pseudo
+	// in: string
+	Discord string `json:"discord"`
+	// Phone number
+	// in: string
+	Phone string `json:"phone"`
+	// University name
+	// in: string
+	University string `json:"university"`
+	// Postal address
+	// in: string
+	PostalAddress string `json:"postal_address"`
 	// Status of the volunteer
 	// in: bool
 	Actif bool `json:"actif"`
 	// Does the volunteer has a bureau role
 	// in: bool
 	Bureau bool `json:"bureau"`
+	// Started of date of volunteering
+	// in: string
+	StartedDate string `json:"started_date"`
 }
 
 type Volunteers []Volunteer
 
 func NewVolunteer(vlt *Volunteer) {
-	stmt, _ := TheDb().Prepare("INSERT INTO volunteers (firstname, lastname, email, actif, bureau) VALUES (?,?,?,?,?);")
-	_, err := stmt.Exec(vlt.Firstname, vlt.Lastname, vlt.Email, vlt.Actif, vlt.Bureau)
+	stmt, _ := TheDb().Prepare("INSERT INTO volunteers (firstname, lastname, email, discord, phone, university, postal_address, actif, bureau, started_date) VALUES (?,?,?,?,?,?,?,?,?,?);")
+	_, err := stmt.Exec(vlt.Firstname, vlt.Lastname, vlt.Email, vlt.Discord, vlt.Phone, vlt.University, vlt.PostalAddress, vlt.Actif, vlt.Bureau, vlt.StartedDate)
 	if err != nil {
 		TheLogger().LogError("volunteer", "can't create new volunteer.", err)
 	}
@@ -36,7 +51,7 @@ func FindVolunteerById(id int) *Volunteer {
 	var vlt Volunteer
 
 	row := TheDb().QueryRow("SELECT * FROM volunteers WHERE id = ?;", id)
-	err := row.Scan(&vlt.Id, &vlt.Firstname, &vlt.Lastname, &vlt.Email, &vlt.Actif, &vlt.Bureau)
+	err := row.Scan(&vlt.Id, &vlt.Firstname, &vlt.Lastname, &vlt.Email, &vlt.Discord, &vlt.Phone, &vlt.University, &vlt.PostalAddress, &vlt.Actif, &vlt.Bureau, &vlt.StartedDate)
 
 	if err != nil {
 		TheLogger().LogWarning("volunteer", "volunteer id not found.", err)
@@ -60,7 +75,7 @@ func AllVolunteers() *Volunteers {
 	for rows.Next() {
 		var vlt Volunteer
 
-		err := rows.Scan(&vlt.Id, &vlt.Firstname, &vlt.Lastname, &vlt.Email, &vlt.Actif, &vlt.Bureau)
+		err := rows.Scan(&vlt.Id, &vlt.Firstname, &vlt.Lastname, &vlt.Email, &vlt.Discord, &vlt.Phone, &vlt.University, &vlt.PostalAddress, &vlt.Actif, &vlt.Bureau, &vlt.StartedDate)
 
 		if err != nil {
 			TheLogger().LogError("volunteer", "volunteers not found.", err)
@@ -73,13 +88,13 @@ func AllVolunteers() *Volunteers {
 }
 
 func UpdateVolunteer(vlt *Volunteer) {
-	stmt, err := TheDb().Prepare("UPDATE volunteers SET firstname=?, lastname=?, email=?, actif=?, bureau=? WHERE id=?;")
+	stmt, err := TheDb().Prepare("UPDATE volunteers SET firstname=?, lastname=?, email=?, discord=?, phone=?, university=?, postal_address=?, actif=?, bureau=?, started_date=? WHERE id=?;")
 
 	if err != nil {
 		TheLogger().LogError("volunteer", "problem with the db.", err)
 	}
 
-	_, err = stmt.Exec(vlt.Firstname, vlt.Lastname, vlt.Email, vlt.Actif, vlt.Bureau, vlt.Id)
+	_, err = stmt.Exec(vlt.Firstname, vlt.Lastname, vlt.Email, vlt.Discord, vlt.Phone, vlt.University, vlt.PostalAddress, vlt.Actif, vlt.Bureau, vlt.StartedDate, vlt.Id)
 
 	if err != nil {
 		TheLogger().LogError("volunteer", "volunteer can't be updated.", err)
