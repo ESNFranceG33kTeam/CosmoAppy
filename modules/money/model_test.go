3fb7c296a5c0b186e64b8788cf0975d8d745276f
Mica <mika89700@gmail.com>
2023-01-24 12:22:57 +0100

    add payment date

--- modules/money/model_test.go
+++ modules/money/model_test.go
@@ -6,13 +6,13 @@ import (
 )
 
 func setUpModel() {
-	NewMoney(&Money{Label: "ESNcard", Price: 5})
-	NewMoney(&Money{Label: "Event", Price: 2})
-	NewMoney(&Money{Label: "Travel", Price: 15})
+	NewMoney(&Money{Label: "ESNcard", Price: 5, PaymentDate: "2023-01-10"})
+	NewMoney(&Money{Label: "Event", Price: 2, PaymentDate: "2023-01-10"})
+	NewMoney(&Money{Label: "Travel", Price: 15, PaymentDate: "2023-01-10"})
 }
 
 func TestNewMoney(t *testing.T) {
-	NewMoney(&Money{Label: "ESNcard", Price: 5})
+	NewMoney(&Money{Label: "ESNcard", Price: 5, PaymentDate: "2023-01-10"})
 }
 
 func TestFindMoneybyLabel(t *testing.T) {
--- modules/money/specimen.go
+++ modules/money/specimen.go
@@ -1,25 +1,32 @@
 package money
 
+import "time"
+
+func payment_date(paymentdate string) string {
+	datedate, _ := time.Parse("2006-01-02", paymentdate)
+	payment_date := datedate.Format("2006-01-02")
+
+	return payment_date
+}
+
 func Specimen() {
-	NewMoney(&Money{Label: "ESNcard", Price: 5})
-	NewMoney(&Money{Label: "ESNcard", Price: 5})
+	NewMoney(&Money{Label: "Adhesion", Price: 1, PaymentDate: payment_date("2021-04-24")})
+	NewMoney(&Money{Label: "Adhesion", Price: 1, PaymentDate: payment_date("2022-04-24")})
+	NewMoney(&Money{Label: "Adhesion", Price: 1, PaymentDate: payment_date("2022-04-24")})
+	NewMoney(&Money{Label: "Adhesion", Price: 1, PaymentDate: payment_date("2022-04-24")})
+	NewMoney(&Money{Label: "Adhesion", Price: 1, PaymentDate: payment_date("2022-04-24")})
+	NewMoney(&Money{Label: "Adhesion", Price: 1, PaymentDate: payment_date("2022-04-24")})
+	NewMoney(&Money{Label: "Adhesion", Price: 1, PaymentDate: payment_date("2022-04-24")})
+	NewMoney(&Money{Label: "Adhesion", Price: 1, PaymentDate: payment_date("2022-04-24")})
+	NewMoney(&Money{Label: "Adhesion", Price: 1, PaymentDate: payment_date("2022-04-24")})
+	NewMoney(&Money{Label: "Adhesion", Price: 1, PaymentDate: payment_date("2022-04-24")})
 
-	NewMoney(&Money{Label: "Adhesion", Price: 1})
-	NewMoney(&Money{Label: "Adhesion", Price: 1})
-	NewMoney(&Money{Label: "Adhesion", Price: 1})
-	NewMoney(&Money{Label: "Adhesion", Price: 1})
-	NewMoney(&Money{Label: "Adhesion", Price: 1})
-	NewMoney(&Money{Label: "Adhesion", Price: 1})
-	NewMoney(&Money{Label: "Adhesion", Price: 1})
-	NewMoney(&Money{Label: "Adhesion", Price: 1})
-	NewMoney(&Money{Label: "Adhesion", Price: 1})
+	NewMoney(&Money{Label: "ESNcard", Price: 5, PaymentDate: payment_date("2022-04-24")})
+	NewMoney(&Money{Label: "ESNcard", Price: 5, PaymentDate: payment_date("2022-04-24")})
 
-	NewMoney(&Money{Label: "Event", Price: 120})
-	NewMoney(&Money{Label: "Event", Price: 0})
-	NewMoney(&Money{Label: "Event", Price: 120})
-	NewMoney(&Money{Label: "Event", Price: 120})
-	NewMoney(&Money{Label: "Event", Price: 80})
-	NewMoney(&Money{Label: "Event", Price: 80})
-	NewMoney(&Money{Label: "Event", Price: 0})
-	NewMoney(&Money{Label: "Event", Price: 0})
+	NewMoney(&Money{Label: "Event", Price: 120, PaymentDate: payment_date("2022-04-24")})
+	NewMoney(&Money{Label: "Event", Price: 120, PaymentDate: payment_date("2022-04-24")})
+	NewMoney(&Money{Label: "Event", Price: 120, PaymentDate: payment_date("2022-04-24")})
+	NewMoney(&Money{Label: "Event", Price: 80, PaymentDate: payment_date("2022-04-24")})
+	NewMoney(&Money{Label: "Event", Price: 80, PaymentDate: payment_date("2022-04-24")})
 }
